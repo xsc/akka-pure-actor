927c259a07ee1fdf08f179c989aa87ce6552a8fb
Yannick Scherer <yannick.scherer@gmail.com>
2017-11-18 13:05:25 +0100

    improve and DRY up persistent actor testcases.

--- src/test/scala/org/xsc/pure/PureLogic.scala
+++ src/test/scala/org/xsc/pure/PureLogic.scala
@@ -5,9 +5,13 @@ object PureLogic {
   sealed trait Effect
   case class Increment(by: Int = 1) extends Effect
   case class Decrement(by: Int = 1) extends Effect
-  case class Print(message: String) extends Effect
+  case class Print(message: String)
+    extends Effect
+    with PurePersistentActor.Ephemeral
 
-  final case class State(value: Int) {
+  final case class State(value: Int,
+                         printMessage: Option[String] = None,
+                         printCount: Int = 0) {
     private def updateValue(f: Int => Int) =
       this.copy(value = f(this.value))
 
@@ -15,7 +19,9 @@ object PureLogic {
       effect match {
         case Increment(by) => this.updateValue(_ + by)
         case Decrement(by) => this.updateValue(_ - by)
-        case Print(_) => this
+        case Print(message) =>
+          this.copy(printMessage = Some(message),
+                    printCount = this.printCount + 1)
       }
     }
   }
@@ -25,12 +31,14 @@ object PureLogic {
   }
 
   sealed trait Action
+  final case class Init(value: Int) extends Action
   final case object Double extends Action
   final case object Fail extends Action
   final case object SayHello extends Action
 
   def handleAction(state: State, action: Action): (Option[String], List[Effect]) = {
     action match {
+      case Init(v) => (None, List(Decrement(state.value), Increment(v)))
       case Double => (None, List(Increment(state.value)))
       case Fail => (Some("It failed."), List.empty)
       case SayHello => (None, List(Print("Hello World!")))
@@ -39,6 +47,11 @@ object PureLogic {
 
   var printed: Option[String] = None
   var printCount = 0
+
+  def resetPrinted(): Unit = {
+    printed = None
+    printCount = 0
+  }
 
   def propagateEffect: PartialFunction[Effect, Future[Unit]] = {
     case Print(message) =>
